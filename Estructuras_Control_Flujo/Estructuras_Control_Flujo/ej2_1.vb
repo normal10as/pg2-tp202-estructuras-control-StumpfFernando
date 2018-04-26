531996b2f13fe4adfda75ad4c6766538f9bf8cec
Stumpf Fernando <stumpffernando88@gmail.com>
2018-04-26 15:31:42 -0300

    2.1

---
+++ Estructuras_Control_Flujo/Estructuras_Control_Flujo/ej2_1.vb
@@ -0,0 +1,19 @@
+﻿Module ej2_1
+
+    Sub Main()
+        Dim a, b As Int16
+        Console.WriteLine("Ingrese valor de A")
+        a = Console.ReadLine()
+        Console.WriteLine("Ingrese valor de B")
+        b = Console.ReadLine()
+        If a = b Then
+            Console.WriteLine("A es igual a B")
+        ElseIf a < b Then
+            Console.WriteLine("A es menor que B")
+        Else
+            Console.WriteLine("A es mayor que B")
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
