a926a809ed55ea755eced6a34d93948d99e935fe
Stumpf Fernando <stumpffernando88@gmail.com>
2018-05-07 21:37:08 -0300

    2.7

---
+++ Estructuras_Control_Flujo/Estructuras_Control_Flujo/ej2_6.vb
@@ -0,0 +1,31 @@
+﻿Module ej2_6
+
+
+
+    Sub Main()
+        Dim des As Single
+        Dim precio, cant, subtotal As Integer
+        Console.WriteLine("Cantidad:")
+        cant = Console.ReadLine()
+        Console.WriteLine("Precio unitario:")
+        precio = Console.ReadLine()
+        Select Case cant
+            Case 10 To 50
+                des = 0.05
+            Case 51 To 250
+                des = 0.1
+            Case Is > 251
+                des = 0.2
+            Case Else
+                des = 0%
+        End Select
+        subtotal = cant * precio
+        Console.WriteLine("El porcentaje de descuento aplicado es del {0} %", des * 100)
+        Console.WriteLine("Subtotal....................... " & subtotal)
+        Console.WriteLine("Monto descontado............... {0} ", subtotal * des)
+        Console.WriteLine("Total a pagar.................. {0} ", subtotal - (subtotal * des))
+        Console.ReadKey()
+    End Sub
+
+
+End Module
---
+++ Estructuras_Control_Flujo/Estructuras_Control_Flujo/ej2_7.vb
@@ -0,0 +1,24 @@
+﻿Module ej2_7
+    Sub main()
+        Dim angulo As Int16
+        Dim grado As String
+        Console.WriteLine("Ingrese el tamaño del angulo")
+        angulo = Console.ReadLine()
+        Select Case angulo
+            Case 1 To 90
+                grado = "Agudo"
+            Case Is = 90
+                grado = "Recto"
+            Case 91 To 179
+                grado = "Obtuso"
+            Case Is = 180
+                grado = "Llano"
+            Case 181 To 359
+                grado = "Concavo"
+            Case Else
+                Console.WriteLine("ERROR...")
+        End Select
+        Console.WriteLine("El angulo es {0}", grado)
+        Console.ReadKey()
+    End Sub
+End Module
